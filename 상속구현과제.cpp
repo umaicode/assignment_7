e038da6ce34e474b54b60f8202cabf04fdbb56c9
umaicode <park.seungchan99@gmail.com>
2023-04-25 10:51:21 +0900

    230425

---
+++ 상속구현과제.cpp
@@ -0,0 +1,76 @@
+//7장 클래스 상속 과제
+#include <iostream>
+#include <string>
+using namespace std;
+class Dept {
+	char dname[10];
+	char* city;
+
+public:
+    int operator>(Dept&); // 정렬할 때 이용
+    Dept(char* dname, char* city);
+	ostream& operator<<(Dept&);
+};
+class Person {
+private:
+	string pid;
+	string pname;
+	ostream& operator<<(Person&);
+
+};
+class Employee : protected Person {
+private:
+	char* eno;
+	Dept* dname;
+protected: 
+	void MoveDept(Dept* dname);
+public:
+    int operator>(Employee&); // 정렬할 때 이용
+    Employee(char* eno, Dept* dname, char* pname, char* pid) {};
+	ostream& operator<<(Employee&);
+
+};
+class Coder : private Employee {
+private:
+	char* language;
+	double workYears;
+public:
+	int operator > (Coder&);// 정렬할 때 이용
+	Coder(char* language, double workYears, char* eno, Dept* dname, string pid, string pname);
+	void MoveDept(Dept* dname);//부서 이동
+	ostream& operator<<(Coder&);
+
+};
+int main() {
+    while (1)
+    {
+        Coder *codings[20];
+
+        int select;
+        //생성자 사용에 의한 입력은 new Coder("이름", ...)으로 값을 지정하여 입력
+        cout << "\n선택 1: 객체 4개 입력(cin 사용), 2. 객체 10개 입력(생성자 사용), 3: 객체 정렬(Dept 이름)과 출력,4. 객체 정렬(Coder workYears)과 출력, 5. 종료" << endl;
+
+        cin >> select;
+        switch (select) {
+        case 1://1: 객체 4개 입력(cin 사용)
+
+            break;
+        case 2://2. 객체 10개 입력(생성자 사용)
+            codings[o] = new Coder("Hello", 15, "E1", "cs"); // cs를 바로 쓰면 안되고 new 를 사용해야 한다.
+            break;
+
+        case 3://3: 객체 정렬(Dept 이름)과 출력
+
+            break;
+        case 4://4. 객체 정렬(Coder workYears)과 출력
+
+            break;
+
+        default:
+            exit(0);
+            break;
+        }
+    }
+    system("pause");
+    return 1;
+}
