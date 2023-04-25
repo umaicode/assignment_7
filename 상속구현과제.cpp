f798bb0d8571125b39716905ade2ecc7c5876408
umaicode <park.seungchan99@gmail.com>
2023-04-25 12:08:07 +0900

    230425

--- 상속구현과제.cpp
+++ 상속구현과제.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 class Dept {
 	char dname[10];
 	char* city;
@@ -11,13 +12,17 @@ public:
     Dept(char* dname, char* city);
 	ostream& operator<<(Dept&);
 };
+
 class Person {
 private:
 	string pid;
 	string pname;
 	ostream& operator<<(Person&);
+public:
+    Person(string pid, string pname) {};
 
 };
+
 class Employee : protected Person {
 private:
 	char* eno;
@@ -26,21 +31,37 @@ protected:
 	void MoveDept(Dept* dname);
 public:
     int operator>(Employee&); // 정렬할 때 이용
-    Employee(char* eno, Dept* dname, char* pname, char* pid) {};
+    Employee(char* eno, Dept* dname, char* pname, char* pid) : Person(pid, pname) {};
 	ostream& operator<<(Employee&);
 
 };
+
 class Coder : private Employee {
 private:
 	char* language;
 	double workYears;
 public:
-	int operator > (Coder&);// 정렬할 때 이용
+    int operator > (Coder&) {
+        // 정렬할 때 이용
+        Employee e = (Employee)(*this); // this는 Coder 객체임.
+        // 여기에스 dept 객체가 없으니까 upcasting 해야한다.
+        if (e > e1) {
+            // 결국에 > 연산자를 재사용해야 한다.
+            // 상위 클래스에 정의된 비교연산자를 재사용.
+        }
+    }
 	Coder(char* language, double workYears, char* eno, Dept* dname, string pid, string pname);
 	void MoveDept(Dept* dname);//부서 이동
 	ostream& operator<<(Coder&);
 
 };
+
+void SortCoder(Coder* []p, int n) {
+    for () {
+        if (p[i] > p[j]) {}
+    }
+}
+
 int main() {
     while (1)
     {
@@ -63,7 +84,7 @@ int main() {
 
             break;
         case 4://4. 객체 정렬(Coder workYears)과 출력
-
+            SortCoder(coding, 14);
             break;
 
         default:
