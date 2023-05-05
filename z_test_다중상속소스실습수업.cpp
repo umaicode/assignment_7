8ce209d7303b9468f0a594e0fc38bd0388dd9192
umaicode <park.seungchan99@gmail.com>
2023-05-06 06:23:32 +0900

    finished

--- z_test_다중상속소스실습수업.cpp
+++ z_test_다중상속소스실습수업.cpp
@@ -12,57 +12,96 @@ private:
     string pname;
 public:
     virtual void print() { cout << pname; }
+    Person() {}
     Person(string pname):pname(pname) {}
 
+    string getPname() {
+        return pname;
+    }
 };
 
 ostream& operator <<(ostream& os, Person& person) {
-
+    os << person.getPname();
+    return os;
 }
+
 class Employee : virtual public Person {
 private:
     string eno;
 public:
     void print() { cout << eno; }
+    Employee() {}
+    Employee(string eno):eno(eno) {}
     Employee(string eno, string pname):Person(pname), eno(eno){}
-
+    string getEno() {
+        return eno;
+    }
+    int Employee::operator>(Employee& other) {
+        return this->eno > other.eno;
+    }
+    friend ostream& operator<<(ostream& os, Employee& employee);
 };
 ostream& operator<<(ostream& os, Employee& employee) {
-
+    os << employee.getEno();
+    return os;
 }
 
 class Student : virtual public Person {
 private:
     string sno;
 public:
+    Student() {}
+    Student(string sno):sno(sno) {}
     Student(string sno, string pname):Person(pname), sno(sno){}
-
+    int Student::operator>(Student& other) {
+        return this->sno > other.sno;
+    }
+    string getSno() {
+        return sno;
+    }
+    friend ostream& operator<<(ostream& os, Student& student);
 };
-ostream& operator<<(ostream& os, Student& st) {
- 
+
+ostream& operator<<(ostream& os, Student& student) {
+    os << student.getSno();
+    return os;
 }
+
 class WorkStudent : public Student, public Employee {
     string cowork;
 public:
     WorkStudent() {}
-    WorkStudent(string cowork, string sno, string eno, string pname) :Student(sno), Employee(eno), cowork(cowork) {}
+    WorkStudent(string sno, string pname, string eno, string cowork) :Student(sno), Employee(eno), cowork(cowork) {}
     int operator >(WorkStudent&);
-
+    string getCowork() {
+        return cowork;
+    }
+    friend ostream& operator<<(ostream& os, WorkStudent& ws);
 };
+
 int WorkStudent::operator >(WorkStudent& ws) {
-    Student s = (Student)ws;
+    Student *s = dynamic_cast<Student*>(&ws);
+    Employee* e = dynamic_cast<Employee*>(&ws);
     int result = this->cowork.compare(ws.cowork);
     if (result > 0) return 1;
     else if (result < 0) return -1;
-    else if (Student(*this) > s) return 1;
+    else if (*dynamic_cast<Student*>(this) > *s) return 1;
     else return -1;
 }
+ostream& operator<<(ostream& os, WorkStudent& ws) {
+    os << " " << ws.getSno() << ", " << ws.getPname() << ", " << ws.getEno() << ", " << ws.getCowork() << endl;
+    return os;
+}
+
 class Designer : public Employee {
     string design;
 };
+
 void swap(Person* p[], int i, int j) {
     Person* temp;
-    temp = p[i]; p[i] = p[j]; p[j] = temp;
+    temp = p[i];
+    p[i] = p[j];
+    p[j] = temp;
 }
 
 int main() {
@@ -77,69 +116,80 @@ int main() {
 
         cin >> select;
         switch (select) {
-        case 1://1: WorkStudent 객체 10개 입력과 출력
-            workstudents[0] = new WorkStudent("p1", "hong", "E1", "Coding");
-            workstudents[1] = new WorkStudent("p2", "hee", "E2", "Coding");
-            workstudents[2] = new WorkStudent("p3", "kim", "E3", "Test");
-            workstudents[3] = new WorkStudent("p4", "lee", "E4", "Test");
-            workstudents[4] = new WorkStudent("p5", "ko", "E5", "Design");
-            workstudents[5] = new WorkStudent("p6", "choi", "E6", "Design");
-            workstudents[6] = new WorkStudent("p7", "han", "E7", "Sales");
-            workstudents[7] = new WorkStudent("p8", "na", "E8", "Sales");
-            workstudents[8] = new WorkStudent("p9", "you", "E9", "Account");
-            workstudents[9] = new WorkStudent("p10", "song", "E10", "Production");
-            for (int i = 0; i < 10; i++)
+            case 1://1: WorkStudent 객체 10개 입력과 출력
             {
-                //cout << 
-            }
-            break;
-        case 2:
-            break;
-            designers[0] = new Designer();
-
-        case 3://3: WorkStudent객체 정렬
-            for (int i = 0; i < 10; i++)
-                for (int j = i + 1; j < 10; j++)
+                workstudents[0] = new WorkStudent("p1", "hong", "E1", "Coding");
+                workstudents[1] = new WorkStudent("p2", "hee", "E2", "Coding");
+                workstudents[2] = new WorkStudent("p3", "kim", "E3", "Test");
+                workstudents[3] = new WorkStudent("p4", "lee", "E4", "Test");
+                workstudents[4] = new WorkStudent("p5", "ko", "E5", "Design");
+                workstudents[5] = new WorkStudent("p6", "choi", "E6", "Design");
+                workstudents[6] = new WorkStudent("p7", "han", "E7", "Sales");
+                workstudents[7] = new WorkStudent("p8", "na", "E8", "Sales");
+                workstudents[8] = new WorkStudent("p9", "you", "E9", "Account");
+                workstudents[9] = new WorkStudent("p10", "song", "E10", "Production");
+                for (int i = 0; i < 10; i++)
                 {
-                    // Student* s = (Student)workstudents[i];
-                    // Student* s = dynamic_cast<Student*>(workstudents[i]);
-                    // if (*workstudents[i] > *workstudents[j]) {
-                    //     swap(workstudents, i, j);
-                    // }
-                    WorkStudent* ws1 = dynamic_cast<WorkStudent*>(workstudents[i]);
-                    WorkStudent* ws2 = dynamic_cast<WorkStudent*>(workstudents[j]);
-
-                    if (*ws1 > *ws2) {
-                        swap(workstudents, i, j);
-                    }
+                    cout << *workstudents[i];
                 }
-            for (int i = 0; i < 10; i++)
-            {
-                //cout<<workstudents[i];
+                break;
             }
-            break;
-        case 4:
-            for (int i = 0; i < 10; i++)
-                for (int j = i + 1; j < 10; j++)
+
+            case 2:
+            {
+                designers[0] = new Designer();
+                break;
+            }
+
+            case 3://3: WorkStudent객체 정렬
+            {
+                for (int i = 0; i < 10; i++)
+                    for (int j = i + 1; j < 10; j++)
+                    {
+                        // Student* s = (Student)workstudents[i];
+                        // Student* s = dynamic_cast<Student*>(workstudents[i]);
+                        // if (*workstudents[i] > *workstudents[j]) {
+                        //     swap(workstudents, i, j);
+                        // }
+                        WorkStudent* ws1 = dynamic_cast<WorkStudent*>(workstudents[i]);
+                        WorkStudent* ws2 = dynamic_cast<WorkStudent*>(workstudents[j]);
+
+                        if (*ws1 > *ws2) {
+                            swap(workstudents, i, j);
+                        }
+                    }
+                for (int i = 0; i < 10; i++)
                 {
-                    //designers의 정렬
-                    Designer* ws1 = dynamic_cast<Designer*>(workstudents[i]);
-                    Designer* ws2 = dynamic_cast<Designer*>(workstudents[i]);
-
-                    if(*ws1 > *ws2) {
-                        swap(designers, i, j);
-                    }
+                    cout << i + 1 << ". " << *(workstudents[i]) << endl;
                 }
-
-            for (int i = 0; i < 10; i++)
-            {
-                //cout<<designers[i];
+                break;
             }
-            break;
-        default:
-            exit(0);
 
-        }
+            case 4:
+            {
+                for (int i = 0; i < 10; i++)
+                    for (int j = i + 1; j < 10; j++)
+                    {
+                        //designers의 정렬
+                        Designer* ws1 = dynamic_cast<Designer*>(workstudents[i]);
+                        Designer* ws2 = dynamic_cast<Designer*>(workstudents[i]);
+
+                        if (*ws1 > *ws2) {
+                            swap(designers, i, j);
+                        }
+                    }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    //cout<<designers[i];
+                }
+                break;
+            }
+
+            default:
+                exit(0);
+
+            }
     }
     system("pause");
     return 1;
